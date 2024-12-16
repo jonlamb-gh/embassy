3bbf9815c793f8a78630f36fc3ac2e39a0465297
Jon Lamb <lamb.jon.io@gmail.com>
2024-12-16 04:37:50 -0800

    Disable doc/async-fn warnings

--- embassy-usb/src/lib.rs
+++ embassy-usb/src/lib.rs
@@ -1,6 +1,6 @@
 #![no_std]
 #![doc = include_str!("../README.md")]
-#![warn(missing_docs)]
+#![allow(missing_docs, async_fn_in_trait)]
 
 // This mod MUST go first, so that the others see its macros.
 pub(crate) mod fmt;
